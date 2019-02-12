58a9656075375131196ecfcfce4cbec86aa02b54
Ed Stone <stone_edward@hotmail.com>
2019-02-12 17:11:09 +0000

    formatted receive samples form

--- platerplotter/platerplotter/admin.py
+++ platerplotter/platerplotter/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from platerplotter.models import Gel1004csv, ReveivedSamples, Gel1005csv
 
-# Register your models here.
+class Gel1004csvAdmin(admin.ModelAdmin):
+	model = Gel1004csv
+
+class ReveivedSamplesAdmin(admin.ModelAdmin):
+	model = ReveivedSamples
+
+class Gel1005csvAdmin(admin.ModelAdmin):
+	model = Gel1005csv
+
+admin.site.register(Gel1004csv, Gel1004csvAdmin)
+admin.site.register(ReveivedSamples, ReveivedSamplesAdmin)
+admin.site.register(Gel1005csv, Gel1005csvAdmin)
--- platerplotter/platerplotter/forms.py
+++ platerplotter/platerplotter/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 from django.forms import ModelForm
 from platerplotter.models import ReveivedSamples
@@ -7,3 +8,15 @@ class ReceivedSamplesForm(ModelForm):
 	class Meta:
 		model = ReveivedSamples
 		exclude = ['sampleReceivedDateTime']
+
+	def clean_gmcRackId(self):
+		gmcRackId = self.cleaned_data['gmcRackId']
+		if not re.match(r'^[A-Z]{2}\d{8}$', gmcRackId):
+			raise ValidationError("Invalid rack ID.")
+		return gmcRackId
+
+	def clean_A1(self):
+		a1 = self.cleaned_data['A1']
+		if not re.match(r'^\d{10}$', a1):
+			raise ValidationError("Invalid sample ID.")
+		return a1
