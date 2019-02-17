e0b2b39cf779f8dabdfc2291e1e42b886cb11f7d
efgstone <stone_edward@hotmail.com>
2019-02-17 17:25:48 +0000

    functionality added to produce 1005 and 1008 acks

--- platerplotter/platerplotter/views.py
+++ platerplotter/platerplotter/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
-from platerplotter.models import Gel1004csv, ReceivedSample, Gel1005csv
+from platerplotter.models import Gel1004csv, ReceivedSample, Gel1005csv, Gel1008csv
 from platerplotter.config.load_config import LoadConfig
 from platerplotter.forms import ReceivedSampleForm
 from datetime import datetime
@@ -100,40 +100,98 @@ def sample_acks(request):
 			return render(request, 'index.html', {"gel1004" : gel1004})
 		if 'gel1005' in request.POST:
 			directory = LoadConfig().load()['gel1005path']
+			datetime_now = datetime.now(pytz.timezone('Europe/London'))
 			filename = "ngis_bio_to_gel_samples_received_" + datetime.now(pytz.timezone('Europe/London')).strftime("%y%m%d_%H%M%S") + ".csv"
 			print(filename)
 			gel1004_with_matched_samples = Gel1004csv.objects.filter(receivedSample__isnull = False, gel1005__isnull = True)
 			print(len(gel1004_with_matched_samples))
 			gel1005s = []
+			received_racks = set()
 			for g in gel1004_with_matched_samples:
+				received_racks.add(g.gmcRackId)
+				gel1005csv = Gel1005csv.objects.create(
+					filename=filename,
+					participantId = g.participantId,
+					laboratoryId = g.laboratoryId,
+					sampleReceived = True,
+					sampleReceivedDateTime = g.receivedSample.sampleReceivedDateTime,
+					reportGeneratedDateTime = datetime_now,
+					laboratorySampleId = g.laboratorySampleId)
+				g.gel1005 = gel1005csv
+				g.save()
+				gel1005s.append(gel1005csv)
 				print(g.participantId)
-			path = directory + filename
-			print(path)
+			print(received_racks)
+			for rack in received_racks:
+				rack_received_missing_sample = Gel1004csv.objects.filter(gmcRackId = rack, receivedSample__isnull = True, gel1005__isnull = True)
+				for g in rack_received_missing_sample:
+					gel1005csv = Gel1005csv.objects.create(
+						filename=filename,
+						participantId = g.participantId,
+						laboratoryId = g.laboratoryId,
+						sampleReceived = False,
+						sampleReceivedDateTime = None,
+						reportGeneratedDateTime = datetime_now,
+						laboratorySampleId = g.laboratorySampleId)
+					g.gel1005 = gel1005csv
+					g.save()
+					gel1005s.append(gel1005csv)
+					print(g.participantId)
+			if gel1005s:
+				path = directory + filename
+				with open(path, 'w', newline='') as csvfile:
+					writer = csv.writer(csvfile, delimiter=',',
+						quotechar=',', quoting=csv.QUOTE_MINIMAL)
+					writer.writerow(['Participant ID', 'Laboratory ID',
+						'Sample Received', 'Sample Received DateTime',
+						'DateTime Report Generated', 'Laboratory Sample ID'])
+					for g in gel1005s:
+						writer.writerow([g.participantId, g.laboratoryId,
+							g.sampleReceived, g.sampleReceivedDateTime,
+							g.reportGeneratedDateTime, g.laboratorySampleId])
+		if 'gel1008' in request.POST:
+			rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+			columns = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+			wells = []
+			for row in rows:
+				for column in columns:
+					wells.append(row + column)
+			print(wells)	
+			directory = LoadConfig().load()['gel1008path']
+			datetime_now = datetime.now(pytz.timezone('Europe/London'))
+			filename = "ngis_bio_to_gel_sample_dispatch_" + datetime.now(pytz.timezone('Europe/London')).strftime("%y%m%d_%H%M%S") + ".csv"
+			print(filename)
+			unplated_samples = Gel1004csv.objects.filter(receivedSample__isnull = False, gel1008__isnull = True)
+			print(len(unplated_samples))
+			well_count = 0
+			gel1008s = []
+			for g in unplated_samples:
+				gel1008csv = Gel1008csv.objects.create(
+							filename = filename,
+							participantId = g.participantId,
+							plateId = 'plateID-input',
+							normalisedBiorepositorySampleVolume = None,
+							normalisedBiorepositoryConcentration = None,
+							wellId = wells[well_count],
+							plateConsignmentNumber = '0000000001',
+							plateDateOfDispatch = datetime_now)
+				g.gel1008 = gel1008csv
+				g.save()
+				gel1008s.append(gel1008csv)
+				well_count += 1
+			if gel1008s:
+				path = directory + filename
+				with open(path, 'w', newline='') as csvfile:
+					writer = csv.writer(csvfile, delimiter=',',
+						quotechar=',', quoting=csv.QUOTE_MINIMAL)
+					writer.writerow(['Participant ID', 'Plate ID',
+						'Normalised Biorepository Sample Volume', 'Normalised Biorepository Concentration',
+						'Well ID', 'Plate Consignment Number', 'Plate Date of Dispatch'])
+					for g in gel1008s:
+						writer.writerow([g.participantId, g.plateId,
+							g.normalisedBiorepositorySampleVolume, g.normalisedBiorepositoryConcentration,
+							g.wellId, g.plateConsignmentNumber, g.plateDateOfDispatch])
 
-	all_gel1004 = Gel1004csv.objects.all()
-	all_received_samples = ReceivedSample.objects.all()
-	matches = []
-	partial_matches = []
-	sample_no_gel1004 = []
-	gel1004_no_sample = []
-	for sample in all_received_samples:
-		try:
-			gel1004 = Gel1004csv.objects.get(gmcRackId=sample.gmcRackId, laboratorySampleId=sample.laboratorySampleId)
-			if gel1004.gmcRackWell == sample.gmcRackWell:
-				print('match')
-				gel1004.receivedSample = sample
-				gel1004.save()
-				print(gel1004, gel1004.receivedSample)
-				matches.append((gel1004, sample))
-			else:
-				partial_matches.append((gel1004, sample))
-			all_gel1004 = all_gel1004.remove(gel1004)
-			all_received_samples = all_received_samples.remove(sample)
-		except:
-			print("no match")
-
+	all_gel1004 = Gel1004csv.objects.filter(receivedSample__isnull = True)
 	return render(request, 'sample-acks.html', {
-		"remaining_gel1004" : all_gel1004,
-		"remaining_samples" : all_received_samples,
-		"matches" : matches,
-		"partial_matches" : partial_matches})
+		"remaining_gel1004" : all_gel1004})
