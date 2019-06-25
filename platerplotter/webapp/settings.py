f16900850ccbeed107f19673845ac469e411e365
Ed Stone <stone_edward@hotmail.com>
2019-06-25 15:40:22 +0100

    added cookie handling

--- platerplotter/webapp/settings.py
+++ platerplotter/webapp/settings.py
@@ -106,3 +106,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CSRF_COOKIE_NAME = 'platerplotter_token'
+SESSION_COOKIE_NAME = 'platerplotter_session'
