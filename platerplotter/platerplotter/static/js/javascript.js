d2e7363361b65e0a4affd3f2aac5c8e5555187a9
Ed Stone <stone_edward@hotmail.com>
2019-11-29 16:48:43 +0000

    custom sort applied to racks ready for plating.

--- platerplotter/platerplotter/static/js/javascript.js
+++ platerplotter/platerplotter/static/js/javascript.js
@@ -10,6 +10,54 @@ $(document).ready(function() {
 	});
 });
 
+(function ($) {
+var unique = 0;
+var types = $.fn.dataTable.ext.type;
+ 
+// Using form $.fn.dataTable.enum breaks at least YuiCompressor since enum is
+// a reserved word in JavaScript
+$.fn.dataTable['enum'] = function ( arr ) {
+    var name = 'enum-'+(unique++);
+    var lookup = window.Map ? new Map() : {};
+ 
+    for ( var i=0, ien=arr.length ; i<ien ; i++ ) {
+        lookup[ arr[i] ] = i;
+    }
+ 
+    // Add type detection
+    types.detect.unshift( function ( d ) {
+        return lookup[ d ] !== undefined ?
+            name :
+            null;
+    } );
+ 
+    // Add sorting method
+    types.order[ name+'-pre' ] = function ( d ) {
+        return lookup[ d ];
+    };
+};
+})(jQuery);
+
+
+$.fn.dataTable.enum( [ 'Proband', 'Family', 'Cancer Germline', 'Tumour' ] );
+	$(document).ready(function() {
+	    $('#dataTableReadyForPlating').DataTable({
+	   		"paging": true,
+	   		"order": [[ 1, "asc" ]],
+	   		"processing": true,
+			dom: 'lBfrtip',
+	        buttons: [ {
+	            extend: 'excelHtml5',
+	            title: "ValidationListExport"
+	        },
+	        {
+	            extend: 'csvHtml5',
+	            title: "ValidationListExport"
+	        }],
+	    });
+	});
+
+
 $(document).ready(function() {
     $('#dataTablePag').DataTable({
    		"paging": true,
