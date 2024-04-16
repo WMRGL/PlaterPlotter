77a9486cda8171f5665181987a99adb3c740b5d8
Isaiah Olatunbosun <daezyworld@gmail.com>
2024-04-16 08:55:20 +0100

    added static to urls.py

--- platerplotter/webapp/urls.py
+++ platerplotter/webapp/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 #from django.contrib.auth import views as auth_views
 from notifications.views import import_acks
@@ -32,3 +35,5 @@ urlpatterns = [
     path('discard/', include('discards.urls')),
     path('', import_acks, name='index'),
 ]
+
+urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
