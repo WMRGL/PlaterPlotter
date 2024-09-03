e5ea34977ce4052cb8b3ac42e21103c67246d1c2
Isaiah Olatunbosun <daezyworld@gmail.com>
2024-09-03 12:06:14 +0100

    discard.py

--- discard.py
+++ discard.py
@@ -1,14 +1,9 @@
-import django 
-from django.conf import settings
 
 from datetime import date, datetime
 from django.contrib.auth.models import User
 from django.db import transaction
 from platerplotter.models import Plate, HoldingRack
 from discards.views import is_discard_due
-
-setings.configure()
-django.setup()
 
 user = User.objects.get(username="RGLBIO")
 holding_racks = HoldingRack.objects.filter(discarded=False)
@@ -22,12 +17,13 @@ for holding_rack in holding_racks:
 with transaction.atomic():
     for discard_rack in discard_racks:
         try:
-            obj = HoldingRack.objects.filter(holding_rack_id=discard_rack.holding_rack_id).last()
-            obj.checked_by = user.username
-            obj.discarded = True
-            obj.discarded_by = user
-            obj.discard_date = datetime.now()
-            obj.save()
+            objects = HoldingRack.objects.filter(holding_rack_id=discard_rack.holding_rack_id)
+            for obj in objects:
+		    obj.checked_by = user.username
+		    obj.discarded = True
+		    obj.discarded_by = user
+		    obj.discard_date = datetime.now()
+		    obj.save()
         except Exception as e:
             print(f"Error updating record: {e}")
             break
